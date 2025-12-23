17077af80bb1a955f4ea28b7dfa787939f616cf8
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-12-23 15:01:33 +0100

    callsign into flightstate as its dynamic

--- src/flightdisplay/domain/aircraft.py
+++ src/flightdisplay/domain/aircraft.py
@@ -4,7 +4,6 @@ from typing import Optional
 @dataclass(frozen=True)
 class Aircraft:
     icao24: str
-    callsign: Optional[str] = None
     country: Optional[str] = None
     manufacturer: Optional[str] = None
     model: Optional[str] = None
--- src/flightdisplay/services/flight_service.py
+++ src/flightdisplay/services/flight_service.py
@@ -1,14 +1,23 @@
+from pathlib import Path
 from typing import List
 
 from flightdisplay.domain.aircraft import Aircraft
 from flightdisplay.domain.flight_state import FlightState
 from flightdisplay.domain.position import Position
+from flightdisplay.infrastructure.db import connect
+from flightdisplay.infrastructure.aircraft_repository_sqlite import AircraftRepositorySQLite
 from flightdisplay.infrastructure.opensky_client import OpenSkyClient
 
 
 class FlightService:
-    def __init__(self, client: OpenSkyClient):
+    def __init__(self, client: OpenSkyClient, db_path: Path):
         self.client = client
+        print(db_path)
+        self.conn = connect(db_path)
+        self.repo = AircraftRepositorySQLite(self.conn)
+
+    def close(self) -> None:
+        self.conn.close()
 
     def get_flights_in_bbox(self, bbox) -> List[FlightState]:
         flights = []
@@ -18,8 +27,11 @@ class FlightService:
             return []
 
         for s in os.states:
-            aircraft = Aircraft(icao24=s.icao24,
-                                callsign=s.callsign.strip())
+            aircraft = self.repo.get_by_icao24(s.icao24)
+
+            if aircraft is None:
+                aircraft = Aircraft(icao24=s.icao24)
+
             posi = Position(
                 lat=s.latitude,
                 lon=s.longitude,
@@ -29,6 +41,7 @@ class FlightService:
                 FlightState(
                     aircraft=aircraft,
                     position=posi,
+                    callsign=s.callsign.strip(),
                     velocity=s.velocity,
                     heading=s.true_track
                 )
